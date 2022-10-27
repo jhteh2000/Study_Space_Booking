6dc08f60b2d9fe1fc0f30eec4feae07e1e1bab5e
jhteh2000 <jhteh2000@gmail.com>
2022-10-27 21:00:33 +0100

    Added check if page is loaded fuction

--- script.py
+++ script.py
@@ -13,17 +13,18 @@ def check_current_time(target_time):
         if current_time == target_time:
             break
 
-def auto_booking():
+def auto_booking(hours):
     global posX, posY
 
     posX, posY = pg.locateCenterOnScreen("page_arrow.png", confidence=.8)
     pg.moveTo(posX, posY, duration=0.1)
     pg.moveRel(20, 0, duration=0.1)
     pg.click()
-    pg.moveTo(1305, 1025, duration=0.1)
+    pg.moveRel(968, 184, duration=0.2)
+    check_page_loaded()
     pg.click()
 
-    for count in range(8):
+    for count in range(hours - 1):
         check_slot_status()
         pg.moveRel(90, 0, duration=0.1)
         pg.click()
@@ -39,6 +40,15 @@ def check_slot_status():
             pg.pixelMatchesColor(posX, posY, (204,204,204), tolerance=10):
             break
 
-check_current_time("18:44")
-auto_booking()
+def check_page_loaded():
+    global posX, posY
+
+    # Not Loaded(rgba(255,255,255,255))
+    posX, posY = pg.position()
+    while True:
+        if not pg.pixelMatchesColor(posX, posY, (255,255,255), tolerance=5):
+            break
+
+check_current_time("12:00")
+auto_booking(8)
 print("Operation Ended")
