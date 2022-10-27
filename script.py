d82288a080d63d0b5ea28846f0dcf486af004543
jhteh2000 <jhteh2000@gmail.com>
2022-10-27 18:48:03 +0100

    Added the auto booking function which will initiated when current time is the set target time

--- script.py
+++ script.py
@@ -1,14 +1,44 @@
-import pyautogui as gui
+from tkinter import Y
+import pyautogui as pg
 from datetime import datetime
 from time import sleep
 
-while True:
-    now = datetime.now()
-    current_time = str(now.strftime("%H:%M"))
-    print(current_time)
-    sleep(1)
+def check_current_time(target_time):
+    while True:
+        now = datetime.now()
+        current_time = str(now.strftime("%H:%M"))
+        print(current_time)
+        sleep(0.1)
 
-    if current_time == "15:28":
-        break
+        if current_time == target_time:
+            break
 
-print("operation ended")
+def auto_booking():
+    global posX, posY
+
+    posX, posY = pg.locateCenterOnScreen("page_arrow.png", confidence=.8)
+    pg.moveTo(posX, posY, duration=0.1)
+    pg.moveRel(20, 0, duration=0.1)
+    pg.click()
+    pg.moveTo(1305, 1025, duration=0.1)
+    pg.click()
+
+    for count in range(8):
+        check_slot_status()
+        pg.moveRel(90, 0, duration=0.1)
+        pg.click()
+
+def check_slot_status():
+    global posX, posY
+
+    # Booked(rgba(245,159,22,255))
+    # Unavailable(rgba(204,204,204,255))
+    posX, posY = pg.position()
+    while True:
+        if pg.pixelMatchesColor(posX, posY, (245,159,22), tolerance=10) or \
+            pg.pixelMatchesColor(posX, posY, (204,204,204), tolerance=10):
+            break
+
+check_current_time("18:44")
+auto_booking()
+print("Operation Ended")
